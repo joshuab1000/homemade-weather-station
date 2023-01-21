5d9ea8503b94626b6c94136874fb0a2a6e8917ad
Josh <joshuab1000@yahoo.com>
2023-01-20 23:50:41 -0500

    Calibrate anemometer for anemometer factor

--- wind.py
+++ wind.py
@@ -6,18 +6,26 @@ import time
 RADIUS_CM = 9.0 # Radius of wind speed sensor
 WIND_INTERVAL = 5 # Time gathering counts
 CIRCUMFERENCE_CM = (2 * math.pi) * RADIUS_CM
+ADJUSTMENT = 1.18 # Compensate for "anemometer factor"
+CM_PER_S_TO_KM_PER_HOUR = .036
 
 def spin():
     global wind_count
     wind_count = wind_count + 1
-    
-# Calculate speed in cm/s
+
+def reset_wind():
+    global wind_count
+    wind_count = 0
+
+# Calculate speed in km/h
 def calculate_speed(time_sec):
     global wind_count
 
     rotations = wind_count / 2.0
     dist_cm = CIRCUMFERENCE_CM * rotations
-    speed = dist_cm / time_sec
+    speed = dist_cm / time_sec #Speed in cm/s
+    speed = speed * CM_PER_S_TO_KM_PER_HOUR #Convert speed to km/h
+    speed = speed * ADJUSTMENT
     return(speed)
 
 wind_speed_sensor = Button(5)
@@ -26,4 +34,4 @@ wind_speed_sensor.when_pressed = spin
 while True:
     wind_count = 0
     time.sleep(WIND_INTERVAL)
-    print((calculate_speed(WIND_INTERVAL) * 0.036), "km/h")
+    print((calculate_speed(WIND_INTERVAL)), "km/h")
