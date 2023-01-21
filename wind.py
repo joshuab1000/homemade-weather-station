5cced62b4c98a84e949db5c4f6e860fb54c2d4e3
Josh <joshuab1000@yahoo.com>
2023-01-21 00:09:17 -0500

    Add wind gust measurements

--- wind.py
+++ wind.py
@@ -2,12 +2,15 @@ from gpiozero import Button
 
 import math
 import time
+import statistics
 
 RADIUS_CM = 9.0 # Radius of wind speed sensor
 WIND_INTERVAL = 5 # Time gathering counts
 CIRCUMFERENCE_CM = (2 * math.pi) * RADIUS_CM
 ADJUSTMENT = 1.18 # Compensate for "anemometer factor"
 CM_PER_S_TO_KM_PER_HOUR = .036
+
+store_speeds = []
 
 def spin():
     global wind_count
@@ -23,8 +26,8 @@ def calculate_speed(time_sec):
 
     rotations = wind_count / 2.0
     dist_cm = CIRCUMFERENCE_CM * rotations
-    speed = dist_cm / time_sec #Speed in cm/s
-    speed = speed * CM_PER_S_TO_KM_PER_HOUR #Convert speed to km/h
+    speed = dist_cm / time_sec # Speed in cm/s
+    speed = speed * CM_PER_S_TO_KM_PER_HOUR # Convert speed to km/h
     speed = speed * ADJUSTMENT
     return(speed)
 
@@ -32,6 +35,13 @@ wind_speed_sensor = Button(5)
 wind_speed_sensor.when_pressed = spin
 
 while True:
-    wind_count = 0
-    time.sleep(WIND_INTERVAL)
-    print((calculate_speed(WIND_INTERVAL)), "km/h")
+    start_time = time.time()
+    while time.time() - start_time <= WIND_INTERVAL:
+        reset_wind()
+        time.sleep(WIND_INTERVAL)
+        final_speed = calculate_speed(WIND_INTERVAL) # Overall wind speed
+        store_speeds.append(final_speed) # Add wind speed to list of wind speeds
+
+    wind_gust = max(store_speeds) # Wind gust top speed
+    wind_speed = statistics.mean(store_speeds)
+    print(wind_speed, wind_gust)
